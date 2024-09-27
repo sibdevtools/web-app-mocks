f5db0d91b8bf2e91074abb0f8737c1a24465f476
sibmaks <sibmaks@vk.com>
2024-09-27 15:49:21 +0300

    Module federation

--- web-app-frontend/src/App.tsx
+++ web-app-frontend/src/App.tsx
@@ -7,7 +7,6 @@ import ServiceListPage from './pages/service/ServiceListPage';
 import AddServiceForm from './pages/service/AddServicePage';
 import EditServicePage from './pages/service/EditServicePage';
 import ServiceMocksListPage from './pages/service/ServiceMocksListPage';
-import { contextPath } from './const/common.const';
 import AddMockPage from './pages/mock/AddMockPage';
 import EditMockPage from './pages/mock/EditMockPage';
 
@@ -16,20 +15,18 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={contextPath}>
-          <Route index element={<ServiceListPage />} />
-          <Route path="service">
-            <Route path={'add'} element={<AddServiceForm />} />
-            <Route path={'edit'}>
-              <Route path={':serviceId'} element={<EditServicePage />} />
-            </Route>
-            <Route path={':serviceId'}>
-              <Route path={'mocks'}>
-                <Route index element={<ServiceMocksListPage />} />
-                <Route path={'add'} element={<AddMockPage />} />
-                <Route path={'edit'}>
-                  <Route path={':mockId'} element={<EditMockPage />} />
-                </Route>
+        <Route index element={<ServiceListPage />} />
+        <Route path="service">
+          <Route path={'add'} element={<AddServiceForm />} />
+          <Route path={'edit'}>
+            <Route path={':serviceId'} element={<EditServicePage />} />
+          </Route>
+          <Route path={':serviceId'}>
+            <Route path={'mocks'}>
+              <Route index element={<ServiceMocksListPage />} />
+              <Route path={'add'} element={<AddMockPage />} />
+              <Route path={'edit'}>
+                <Route path={':mockId'} element={<EditMockPage />} />
               </Route>
             </Route>
           </Route>
