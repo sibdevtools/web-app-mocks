dde29ce0a383840cd03687983c1e13e3086a56f4
sibmaks <sibmaks@vk.com>
2025-02-21 19:53:16 +0300

    Add documentation for mocking python & JS #17 - add response examples

--- web-app-frontend/src/pages/mock/CodeDocumentation.tsx
+++ web-app-frontend/src/pages/mock/CodeDocumentation.tsx
@@ -121,27 +121,108 @@ const rqCookiesExamples = [
     description: 'Get request cookie',
     implementations: {
       javascript: `const rq = wam.request();
+const cookie = rq.cookie('cookie1');`,
+      python: `rq = wam.request()
+cookie = rq.cookie("cookie1")`,
+    }
+  },
+  {
+    description: 'Cookie domain',
+    implementations: {
+      javascript: `const rq = wam.request();
 const cookie = rq.cookie('cookie1');
-cookie.getDomain(); // cookie domain
-cookie.getMaxAge(); // cookie max age
-cookie.getPath(); // cookie path
-cookie.getSecure(); // cookie secure flag
-cookie.getName(); // cookie name
-cookie.getValue(); // cookie value
-cookie.isHttpOnly(); // http only cookie flag
-cookie.getAttribute("attributeKey"); // get attribute value
-cookie.getAttributes(); // get attributes map`,
+cookie.getDomain();`,
       python: `rq = wam.request()
 cookie = rq.cookie("cookie1")
-cookie.getDomain() # cookie domain
-cookie.getMaxAge() # cookie max age
-cookie.getPath() # cookie path
-cookie.getSecure() # cookie secure flag
-cookie.getName() # cookie name
-cookie.getValue() # cookie value
-cookie.isHttpOnly() # http only cookie flag
-cookie.getAttribute("attributeKey") # get attribute value
-cookie.getAttributes() # get attributes map`,
+cookie.getDomain()`,
+    }
+  },
+  {
+    description: 'Cookie max age',
+    implementations: {
+      javascript: `const rq = wam.request();
+const cookie = rq.cookie('cookie1');
+cookie.getMaxAge();`,
+      python: `rq = wam.request()
+cookie = rq.cookie("cookie1")
+cookie.getMaxAge()`,
+    }
+  },
+  {
+    description: 'Cookie path',
+    implementations: {
+      javascript: `const rq = wam.request();
+const cookie = rq.cookie('cookie1');
+cookie.getPath();`,
+      python: `rq = wam.request()
+cookie = rq.cookie("cookie1")
+cookie.getPath()`,
+    }
+  },
+  {
+    description: 'Cookie secure flag',
+    implementations: {
+      javascript: `const rq = wam.request();
+const cookie = rq.cookie('cookie1');
+cookie.getSecure();`,
+      python: `rq = wam.request()
+cookie = rq.cookie("cookie1")
+cookie.getSecure()`,
+    }
+  },
+  {
+    description: 'Cookie name',
+    implementations: {
+      javascript: `const rq = wam.request();
+const cookie = rq.cookie('cookie1');
+cookie.getName();`,
+      python: `rq = wam.request()
+cookie = rq.cookie("cookie1")
+cookie.getName()`,
+    }
+  },
+  {
+    description: 'Cookie value',
+    implementations: {
+      javascript: `const rq = wam.request();
+const cookie = rq.cookie('cookie1');
+cookie.getValue();`,
+      python: `rq = wam.request()
+cookie = rq.cookie("cookie1")
+cookie.getValue()`,
+    }
+  },
+  {
+    description: 'Cookie is HTTP only flag',
+    implementations: {
+      javascript: `const rq = wam.request();
+const cookie = rq.cookie('cookie1');
+cookie.isHttpOnly();`,
+      python: `rq = wam.request()
+cookie = rq.cookie("cookie1")
+cookie.isHttpOnly()`,
+    }
+  },
+  {
+    description: 'Cookie get attribute value',
+    implementations: {
+      javascript: `const rq = wam.request();
+const cookie = rq.cookie('cookie1');
+const value = cookie.getAttribute("attributeKey");`,
+      python: `rq = wam.request()
+cookie = rq.cookie("cookie1")
+value = cookie.getAttribute("attributeKey")`,
+    }
+  },
+  {
+    description: 'Cookie get attributes map',
+    implementations: {
+      javascript: `const rq = wam.request();
+const cookie = rq.cookie('cookie1');
+const attributes = cookie.getAttributes();`,
+      python: `rq = wam.request()
+cookie = rq.cookie("cookie1")
+attributes = cookie.getAttributes()`,
     }
   },
 ];
@@ -175,6 +256,278 @@ body = rq.json()`,
   },
 ];
 
+const rsExamples = [
+  {
+    description: 'Get response object',
+    implementation: `wam.response()`,
+  },
+  {
+    description: 'Set response status',
+    implementations: {
+      javascript: `const rs = wam.response();
+rs.status(503);`,
+      python: `rs = wam.response()
+rs.status(503)`
+    }
+  },
+  {
+    description: 'Set response header',
+    implementations: {
+      javascript: `const rs = wam.response();
+rs.header('key-string', 'value-string');`,
+      python: `rs = wam.response()
+rs.header("key-string", "value-string")`
+    }
+  },
+];
+const rsCookieExamples = [
+  {
+    description: 'Add cookie to response. Insecure and HTTP only.',
+    implementations: {
+      javascript: `const rs = wam.response();
+rs.cookie('name', 'value');`,
+      python: `rs = wam.response()
+rs.cookie("name", "value")`
+    }
+  },
+  {
+    description: 'Add cookie to response',
+    implementations: {
+      javascript: `const rs = wam.response();
+const httpOnly = false;
+const secure = true;
+rs.cookie('name', 'value', secure, httpOnly);`,
+      python: `rs = wam.response()
+httpOnly = False
+secure = True
+rs.cookie("name", "value", secure, httpOnly)`
+    }
+  },
+];
+const rsBodyExamples = [
+  {
+    description: 'Write to response text in UTF-8',
+    implementations: {
+      javascript: `const rs = wam.response();
+rs.plain('any-string');`,
+      python: `rs = wam.response()
+rs.plain("any-string")`
+    }
+  },
+  {
+    description: 'Write to response text in UTF-8 and set status and content type header',
+    implementations: {
+      javascript: `const rs = wam.response();
+rs.plain(500, 'text/plain', 'any-string');`,
+      python: `rs = wam.response()
+rs.plain(500, "text/plain", "any-string")`
+    }
+  },
+  {
+    description: 'Write to response bytes array',
+    implementations: {
+      javascript: `const rs = wam.response();
+rs.bytes([48, 49]);`,
+      python: `rs = wam.response()
+rs.bytes([48, 49])`
+    }
+  },
+  {
+    description: 'Write to bytes array and set status and content type header',
+    implementations: {
+      javascript: `const rs = wam.response();
+rs.bytes(500, 'text/plain', [48, 49]);`,
+      python: `rs = wam.response()
+rs.bytes(500, "text/plain", [48, 49])`
+    }
+  },
+  {
+    description: 'Write to response JSON',
+    implementations: {
+      javascript: `const rs = wam.response();
+rs.json({key: 'value'});`,
+      python: `rs = wam.response()
+rs.json({"key": "value"})`,
+    }
+  },
+  {
+    description: 'Write to JSON and set status and content type header',
+    implementations: {
+      javascript: `const rs = wam.response();
+rs.json(500, 'text/plain', {key: 'value'});`,
+      python: `rs = wam.response()
+rs.json(500, "text/plain", {"key": "value"})`
+    }
+  },
+];
+
+const sessionsExamples = [
+  {
+    description: 'Get sessions service',
+    implementation: `wam.sessions()`,
+  },
+  {
+    description: 'Get session by ID with last version',
+    implementations: {
+      javascript: `const sessions = wam.sessions();
+const session = sessions.get('session-id');`,
+      python: `sessions = wam.sessions()
+session = sessions.get("session-id")`
+    }
+  },
+  {
+    description: 'Get session by ID and specific version',
+    implementations: {
+      javascript: `const sessions = wam.sessions();
+const version = 1;
+const session = sessions.get('session-id', version);`,
+      python: `sessions = wam.sessions()
+version = 1
+session = sessions.get("session-id", version)`
+    }
+  },
+  {
+    description: 'Get session by ID and specific version (version is string)',
+    implementations: {
+      javascript: `const sessions = wam.sessions();
+const version = '1';
+const session = sessions.get('session-id', version);`,
+      python: `sessions = wam.sessions()
+version = "1"
+session = sessions.get("session-id", version)`
+    }
+  },
+  {
+    description: 'Create session',
+    implementations: {
+      javascript: `const sessions = wam.sessions();
+const sections = {
+  sectionKey: {
+    sectionAttributeKey: 'sectionAttributeValue',
+    sectionAttributeKey2: true,
+    sectionAttributeKey3: 42,
+    sectionAttributeKey4: ['item1', 'item2'],
+    sectionAttributeKey5: {key: 'value'},
+  }
+};
+const ownerTypeCode = 'SERVICE'; // or 'USER'
+const ownerId = '123456';
+const permissions = ['PERMISSION1', 'PERMISSION2'];
+const session = sessions.create(sections, ownerTypeCode, ownerId, permissions);`,
+      python: `sessions = wam.sessions()
+sections = {
+  "sectionKey": {
+    "sectionAttributeKey": "sectionAttributeValue",
+    "sectionAttributeKey2": True,
+    "sectionAttributeKey3": 42,
+    "sectionAttributeKey4": ["item1", "item2"],
+    "sectionAttributeKey5": {"key": "value"},
+  }
+}
+ownerTypeCode = "SERVICE" # or "USER"
+ownerId = "123456"
+permissions = ["PERMISSION1", "PERMISSION2"]
+session = sessions.create(sections, ownerTypeCode, ownerId, permissions)`,
+    }
+  },
+];
+const sessionIdExamples = [
+  {
+    description: 'Get session id',
+    implementations: {
+      javascript: `const sessions = wam.sessions();
+const session = sessions.get('session-id');
+const sessionId = session.getId();`,
+      python: `sessions = wam.sessions()
+session = sessions.get("session-id")
+sessionId = session.getId()`
+    }
+  },
+  {
+    description: 'Get session uid',
+    implementations: {
+      javascript: `const sessions = wam.sessions();
+const session = sessions.get('session-id');
+const sessionId = session.getId();
+const sessionUID = sessionId.getUID();`,
+      python: `sessions = wam.sessions()
+session = sessions.get("session-id")
+sessionId = session.getId()
+sessionUID = sessionId.getUID()`
+    }
+  },
+  {
+    description: 'Get session version',
+    implementations: {
+      javascript: `const sessions = wam.sessions();
+const session = sessions.get('session-id');
+const sessionId = session.getId();
+const sessionVersion = sessionId.getVersion();`,
+      python: `sessions = wam.sessions()
+session = sessions.get("session-id")
+sessionId = session.getId()
+sessionVersion = sessionId.getVersion()`
+    }
+  }
+];
+const sessionAttributesExamples = [
+  {
+    description: 'Get attribute value',
+    implementations: {
+      javascript: `const sessions = wam.sessions();
+const session = sessions.get('session-id');
+const attributeValue = session.get('sectionKey', 'attributeKey');`,
+      python: `sessions = wam.sessions()
+session = sessions.get("session-id")
+attributeValue = session.get("sectionKey", "attributeKey")`
+    }
+  },
+  {
+    description: 'Get attribute names',
+    implementations: {
+      javascript: `const sessions = wam.sessions();
+const session = sessions.get('session-id');
+const attributesNames = session.getAttributeNames('sectionKey');`,
+      python: `sessions = wam.sessions()
+session = sessions.get("session-id")
+attributesNames = session.getAttributeNames("sectionKey")`
+    }
+  },
+  {
+    description: 'Create attribute, fail if attribute already exists',
+    implementations: {
+      javascript: `const sessions = wam.sessions();
+const session = sessions.get('session-id');
+session.add('sectionKey', 'attributeKey1', 'attributeValue');`,
+      python: `sessions = wam.sessions()
+session = sessions.get("session-id")
+session.add("sectionKey", "attributeKey1", "attributeValue")`
+    }
+  },
+  {
+    description: 'Set attribute, create if attribute does not exist',
+    implementations: {
+      javascript: `const sessions = wam.sessions();
+const session = sessions.get('session-id');
+session.set('sectionKey', 'attributeKey1', 'attributeValue');`,
+      python: `sessions = wam.sessions()
+session = sessions.get("session-id")
+session.set("sectionKey", "attributeKey1", "attributeValue")`
+    }
+  },
+  {
+    description: 'Remove attribute',
+    implementations: {
+      javascript: `const sessions = wam.sessions();
+const session = sessions.get('session-id');
+session.remove('sectionKey', 'attributeKey1');`,
+      python: `sessions = wam.sessions()
+session = sessions.get("session-id")
+session.remove("sectionKey", "attributeKey1")`
+    }
+  },
+];
+
 const allExamples: ExampleSection = {
   name: 'Examples',
   sections: [
@@ -202,23 +555,34 @@ const allExamples: ExampleSection = {
     },
     {
       name: 'Response',
-      examples: rqExamples,
+      examples: rsExamples,
       sections: [
         {
-          name: 'Headers',
-          examples: rqHeadersExamples
-        },
-        {
-          name: 'Query Parameters',
-          examples: rqQueryParamsExamples
-        },
-        {
-          name: 'Cookies',
-          examples: rqCookiesExamples
+          name: 'Cookie',
+          examples: rsCookieExamples
         },
         {
           name: 'Body',
-          examples: rqBodyExamples
+          examples: rsBodyExamples
+        }
+      ]
+    },
+    {
+      name: 'Sessions',
+      examples: sessionsExamples,
+      sections: [
+        {
+          name: 'Session Actions',
+          sections: [
+            {
+              name: 'Session ID',
+              examples: sessionIdExamples
+            },
+            {
+              name: 'Session attributes',
+              examples: sessionAttributesExamples
+            }
+          ]
         }
       ]
     }
--- web-app/src/main/java/com/github/sibdevtools/web/app/mocks/service/handler/impl/graalvm/dto/GraalVMSessions.java
+++ web-app/src/main/java/com/github/sibdevtools/web/app/mocks/service/handler/impl/graalvm/dto/GraalVMSessions.java
@@ -4,11 +4,12 @@ import com.github.sibdevtools.session.api.dto.SessionId;
 import com.github.sibdevtools.session.api.dto.SessionOwnerType;
 import com.github.sibdevtools.session.api.rq.CreateSessionRq;
 import com.github.sibdevtools.session.api.service.SessionService;
+import com.github.sibdevtools.web.app.mocks.service.handler.impl.graalvm.GraalVMConverter;
 import jakarta.annotation.Nonnull;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.graalvm.polyglot.HostAccess;
 
-import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
@@ -16,6 +17,7 @@ import java.util.Map;
  * @author sibmaks
  * @since 0.0.1
  */
+@Slf4j
 @AllArgsConstructor
 public class GraalVMSessions {
     private final SessionService sessionService;
@@ -43,13 +45,13 @@ public class GraalVMSessions {
     }
 
     @HostAccess.Export
-    public GraalVMSession create(@Nonnull Map<String, Map<String, Serializable>> sections,
+    public GraalVMSession create(@Nonnull Map<String, Map<String, ?>> sections,
                                  @Nonnull String ownerTypeCode,
                                  @Nonnull String ownerId,
                                  @Nonnull List<String> permissions) {
         var ownerType = SessionOwnerType.valueOf(ownerTypeCode);
         var rq = CreateSessionRq.builder()
-                .sections(sections)
+                .sections(GraalVMConverter.convertSections(sections))
                 .ownerType(ownerType)
                 .ownerId(ownerId)
                 .permissions(permissions)
